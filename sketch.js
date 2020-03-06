d2547ebe982e3d795fe1bb3c0e219b0416109507
AdityaKantipudi <61592967+AdityaKantipudi@users.noreply.github.com>
2020-03-06 20:56:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,6 +31,7 @@ function draw() {
     gameObject2.shapeColor="green";
     movingRect.shapeColor="green"
   }
+  bounceOff(gameObject1,gameObject2);
   
   drawSprites(); 
   
@@ -47,3 +48,30 @@ function isTouching(object1,object2)
     return false;
   }
 }
+function bounceOff(object1,object2){
+  if(object1.x-object2.x<object1.width/2+object2.width/2&&
+    object2.x-object1.x<object1.width/2+object2.width/2){
+  object1.velocityX = object1.velocityX * (-1);
+    object2.velocityX = object2.velocityX * (-1)
+  }
+  if (object1.y-object2.y<object1.width/2+object2.width/2&&
+    object2.y-object1.y<object1.width/2+object2.width/2){
+  object1.velocityY = object1.velocityY * (-1)
+    object2.velocityY = object2.velocityY * (-1)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
